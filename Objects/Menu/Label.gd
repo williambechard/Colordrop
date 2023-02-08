d08f2377f07164e3f5378365fdee7ab089293cd0
williambechard <ury2ok2000@gmail.com>
2023-02-08 16:35:15 -0600

    audio complete on title screen

--- Objects/Menu/Label.gd
+++ Objects/Menu/Label.gd
@@ -8,17 +8,23 @@ onready var audioPlayer = $"/root/AudioManager"
 
 onready var arrow=load("res://Art/Menu/arrow.png")
 onready var hand=load("res://Art/Menu/hand.png")
+onready var timer = $Timer
 
-
+var blink :bool = false
+var fadeOut :bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(arrow)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+ 
 
+func _physics_process(delta):
+	if(blink):
+		if(fadeOut):
+			set_modulate(lerp(get_modulate(), Color(0,0,0,0), 0.2))
+		else:
+			set_modulate(lerp(get_modulate(), Color(0,0,0,0), 0.2))
 
 func _on_Button_mouse_entered():
 	add_color_override("font_color", Color(1,.5,.5,1))
@@ -28,5 +34,13 @@ func _on_Button_mouse_entered():
 
 func _on_Button_mouse_exited():
 	add_color_override("font_color", Color(1,1,1,1))
- 
 	Input.set_custom_mouse_cursor(arrow, CURSOR_ARROW)
+
+
+func _on_Button_pressed():
+		audioPlayer.play("res://Audio/success1.wav")
+		timer.start(1)
+
+
+func _on_Timer_timeout():
+	fadeOut=!fadeOut
