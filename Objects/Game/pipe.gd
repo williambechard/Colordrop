ce898f5f75e8e613bd2fdced7db67fe03539055b
williambechard <ury2ok2000@gmail.com>
2023-02-17 10:05:23 -0600

    node and pipe generation complete

--- Objects/Game/pipe.gd
+++ Objects/Game/pipe.gd
@@ -4,8 +4,10 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var from : Node2D = null
-onready var to : Node2D = null
+onready var from : Node2D
+onready var to : Node2D
+
+var valve : Node2D
 
 onready var line : Line2D
 
@@ -14,13 +16,13 @@ func _ready():
 	 pass
  
 
-func setPipe(f:Node2D, t:Node2D):	
+func setPipe(f:Node2D, t:Node2D):
+	valve=$Valve
 	line=$Line
 	from = f
 	to = t
 	line.add_point(f.position, 0)
 	line.add_point(t.position, 1)
+	valve.position=Vector2(f.position.x + ((t.position.x-f.position.x)*.5), (f.position.y + ((t.position.y-f.position.y) *.5)))
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
